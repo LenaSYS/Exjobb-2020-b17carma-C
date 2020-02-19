720be3acab2fb037c2556ce32e18276e1040f97e
b17carma <b17carma@student.his.se>
2020-02-19 13:58:07 +0100

    Add save and fetch stubs

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const port = 3001;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/save', (req, res) => res.send('Saved!'));
+app.get('/fetch', (req, res) => res.send('{"test": 100}'));
 
 app.listen(port, () => console.log(`Scanner server listening on port ${port}`));
