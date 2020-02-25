d6070f8731f5ba3133155947741eb914184ad1fe
b17carma <b17carma@student.his.se>
2020-02-25 21:53:49 +0100

    Add environment variables

--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ const equipment = require('./api/routes/equipment');
 const parts = require('./api/routes/parts');
 
 const app = express();
-const port = 3001;
 
 app.use('/equipment', equipment);
 app.use('/parts', parts);
 
-app.listen(port, () => console.log(`Scanner server listening on port ${port}`));
+require('dotenv').config();
+
+if (process.env.ENVIRONMENT === 'DEVELOPMENT') {
+    app.listen(process.env.PORT, () => console.log(`Scanner server listening on port ${process.env.PORT}`));
+} else if (process.env.ENVIRONMENT === 'PRODUCTION') {
+    let fs = require('fs');
+    let https = require('https');
+
+    let privateKey = fs.readFileSync('/etc/letsencrypt/live/api.carlmaier.se/privkey.pem', 'utf8');
+    let certificateKey = fs.readFileSync('/etc/letsencrypt/live/api.carlmaier.se/fullchain.pem', 'utf8');
+
+    let credentials = {
+        key: privateKey,
+        cert: certificateKey
+    };
+
+    let server = https.createServer(credentials, app);
+    server.listen(process.env.PORT);
+}
