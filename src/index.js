9da9df0e332b9f338e0a5e23c189a56f20c86842
b17carma <b17carma@student.his.se>
2020-02-25 21:57:48 +0100

    Add debug message when running in production mode

--- src/index.js
+++ src/index.js
@@ -24,6 +24,6 @@ if (process.env.ENVIRONMENT === 'DEVELOPMENT') {
         cert: certificateKey
     };
 
-    let server = https.createServer(credentials, app);
+    let server = https.createServer(credentials, app, () => console.log(`Scanner server listening on port ${process.env.PORT}`));
     server.listen(process.env.PORT);
 }
