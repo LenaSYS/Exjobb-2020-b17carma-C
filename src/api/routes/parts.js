617ba3d2b309f724ad50b0dd7815babbd7db3de2
b17carma <b17carma@student.his.se>
2020-02-27 14:37:19 +0100

    Populate lastScan wgen fetching specific parts

--- src/api/routes/parts.js
+++ src/api/routes/parts.js
@@ -16,7 +16,7 @@ router.get('/:equipmentId', function (req, res) {
 router.get('/:equipmentId/:partId', function(req, res) {
     mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 
-    Part.findOne({equipment: req.params.equipmentId, _id: req.params.partId}).exec(function (err, part) {
+    Part.findOne({equipment: req.params.equipmentId, _id: req.params.partId}).populate('lastScan').exec(function (err, part) {
         if (part == null)
             return res.send({});
 
