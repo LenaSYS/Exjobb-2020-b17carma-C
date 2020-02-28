33b164a7ca1526d1579f46267701c0a14374f888
b17carma <b17carma@student.his.se>
2020-02-28 14:46:53 +0100

    Include information about if a part has been scanned

--- src/api/routes/parts.js
+++ src/api/routes/parts.js
@@ -5,30 +5,46 @@ const moment = require('moment');
 const router = express.Router();
 
 const Part = require("../mongodb/schema/Part");
-0
+
+function addScanInfo(parts) {
+    parts.forEach(function (part) {
+        addPartScanInfo(part);
+    });
+}
+
+function addPartScanInfo(part) {
+    if (part.hasOwnProperty('lastScan')) { //Add last scan info in backend to reduce front-end overhead
+        let scanMoment = moment(part.lastScan.time);
+        let currentMoment = moment(new Date());
+
+        part.lastScanToday = currentMoment.isSame(scanMoment, 'day'); //Check if scan was made the same day as today
+    }
+}
+
 router.get('/:equipmentId', function (req, res) {
     mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 
-    Part.find({equipment : req.params.equipmentId}).lean().populate('lastScan').exec(function (err, parts) {
-        parts.forEach(function(part) {
-            if (part.hasOwnProperty('lastScan')) { //Add last scan info in backend to reduce front-end overhead
-                let scanMoment = moment(part.lastScan.time);
-                let currentMoment = moment(new Date());
+    Part.find({equipment: req.params.equipmentId}).lean().populate('lastScan').exec(function (err, parts) {
+        if (parts == null)
+            return res.send({});
 
-                part.lastScanToday = currentMoment.isSame(scanMoment, 'day'); //Check if scan was made the same day as today
-            }
-        });
+        addScanInfo(parts);
 
         return res.send(JSON.stringify(parts));
     });
 });
 
-router.get('/:equipmentId/:partId', function(req, res) {
+router.get('/:equipmentId/:partId', function (req, res) {
     mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 
-    Part.findOne({equipment: req.params.equipmentId, _id: req.params.partId}).populate('lastScan').exec(function (err, part) {
+    Part.findOne({
+        equipment: req.params.equipmentId,
+        _id: req.params.partId
+    }).populate('lastScan').lean().exec(function (err, part) {
         if (part == null)
             return res.send({});
+
+        addPartScanInfo(part);
 
         return res.send(JSON.stringify(part));
     });
