5c417665f3deb8a6f2f7c35876f5322e9b17d686
b17carma <b17carma@student.his.se>
2020-02-28 14:29:38 +0100

    Cleanup code

--- src/api/routes/parts.js
+++ src/api/routes/parts.js
@@ -5,16 +5,17 @@ const moment = require('moment');
 const router = express.Router();
 
 const Part = require("../mongodb/schema/Part");
-
+0
 router.get('/:equipmentId', function (req, res) {
     mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 
-    Part.find({equipment : req.params.equipmentId}).populate('lastScan').exec(function (err, parts) {
-        parts.forEach(function(part, i) {
-            if (parts[i].hasOwnProperty('lastScan')) { //Add last scan info in backend to reduce front-end overhead
+    Part.find({equipment : req.params.equipmentId}).lean().populate('lastScan').exec(function (err, parts) {
+        parts.forEach(function(part) {
+            if (part.hasOwnProperty('lastScan')) { //Add last scan info in backend to reduce front-end overhead
                 let scanMoment = moment(part.lastScan.time);
                 let currentMoment = moment(new Date());
-                parts[i].lastScanToday = currentMoment.isSame(scanMoment, 'day'); //Check if scan was made the same day as today
+
+                part.lastScanToday = currentMoment.isSame(scanMoment, 'day'); //Check if scan was made the same day as today
             }
         });
 
