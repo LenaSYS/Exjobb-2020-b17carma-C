bddb7c17d9274efe2341888f4f505223878d75e3
b17carma <b17carma@student.his.se>
2020-02-28 13:58:09 +0100

    Use standardized time zone when determining if a part has been scanned on the current day

--- src/api/routes/analytics.js
+++ src/api/routes/analytics.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const {isSameDay} = require('date-fns');
+const {isSameDay, utcToZonedTime} = require('date-fns');
 
 const router = express.Router();
 
@@ -33,13 +33,18 @@ router.get('/stats', function (req, res) {
 router.get('/:equipmentId/scans', function (req, res) {
     mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 
+    let timeZone = 'Europe/Berlin';
+
     Part.find({equipment: req.params.equipmentId}).populate('lastScan').lean().exec(function (err, parts) {
         let scanned = 0;
         let totalParts = parts.length;
 
         parts.map(function (part, i) {
             if (part.hasOwnProperty('lastScan')) {
-                if (isSameDay(new Date(), part.lastScan.time)) {
+                let now = utcToZonedTime(new Date(), timeZone);
+                let scanDate = utcToZonedTime(part.lastScan.time, timeZone);
+
+                if (isSameDay(now, scanDate)) {
                     scanned++;
                 }
             }
