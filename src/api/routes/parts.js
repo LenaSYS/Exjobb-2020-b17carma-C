8fdd275cdf46be668ccbd64c4f8726bc1c02395e
b17carma <b17carma@student.his.se>
2020-02-28 14:15:34 +0100

    Use moment instead of date-fns

--- src/api/routes/parts.js
+++ src/api/routes/parts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const router = express.Router();
 
@@ -9,6 +10,14 @@ router.get('/:equipmentId', function (req, res) {
     mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 
     Part.find({equipment : req.params.equipmentId}).populate('lastScan').exec(function (err, parts) {
+        parts.map(function(part, i) {
+            if (part.hasOwnProperty('lastScan')) { //Add last scan info in backend to reduce front-end overhead
+                let scanMoment = moment(part.lastScan.time);
+                let currentMoment = moment(new Date());
+                part.lastScanToday = currentMoment.isSame(scanMoment, 'day'); //Check if scan was made the same day as today
+            }
+        });
+
         return res.send(JSON.stringify(parts));
     });
 });
