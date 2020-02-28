9701c660b6a812ae6ea10a771719e743b308debc
b17carma <b17carma@student.his.se>
2020-02-28 13:30:06 +0100

    Add more analytics

--- src/api/routes/analytics.js
+++ src/api/routes/analytics.js
@@ -8,6 +8,28 @@ let Equipment = require("../mongodb/schema/Equipment");
 let Part = require("../mongodb/schema/Part");
 let Scan = require("../mongodb/schema/Scan");
 
+router.get('/stats', function (req, res) {
+    mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
+
+    let successCount = 0;
+    let failureCount = 0;
+
+    Scan.find().lean().exec(function (err, scans) {
+        scans.map(function (scan) {
+            if (scan.status)
+                successCount++;
+            else
+                failureCount++;
+        });
+
+        return res.send(JSON.stringify({
+            totalScans: scans.length,
+            successCount: successCount,
+            failureCount: failureCount
+        }));
+    });
+});
+
 router.get('/:equipmentId/scans', function (req, res) {
     mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 
