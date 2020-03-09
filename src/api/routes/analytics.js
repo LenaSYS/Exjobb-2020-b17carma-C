42c0f6e6d49fbac01dfcb9542a6a63aa7c6d7e74
b17carma <b17carma@student.his.se>
2020-03-09 15:35:57 +0100

    Include daily status for overall component analytics

--- src/api/routes/analytics.js
+++ src/api/routes/analytics.js
@@ -127,53 +127,51 @@ router.get('/calendar', function (req, res) {
 router.get("/overview", function (req, res) {
     mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 
-    Component.find().populate('equipment').lean().exec(function (err, components) {
+    Component.find().populate('equipment').populate('lastScan').lean().exec(function (err, components) {
         let actionRequiredComponents = [];
 
         let startDate = moment().startOf("month");
         let endDate = moment().endOf("month");
 
-        for (let m = moment(startDate); m.isBefore(endDate); m.add(1, 'days')) {
-            let currentDate = moment(m);
-            let currentDay = currentDate.day();
-            let formattedDay = m.format("DD/MM/YY");
+        Scan.find({}, function (err, scans) {
+            if (err)
+                return console.log(err);
 
-            actionRequiredComponents.push({
-                date: formattedDay,
-                data: []
-            });
+            for (let m = moment(startDate); m.isBefore(endDate); m.add(1, 'days')) {
+                let currentDate = moment(m);
+                let currentDay = currentDate.day();
+                let formattedDay = m.format("DD/MM/YY");
 
-            components.map(function (component, i) {
-                let frequency = component.frequency;
-                let frequencyType = component.frequencyType;
-                let frequencyDays = component.frequencyDays;
-                let frequencyTypeString = frequencyType === 0 ? 'days' : frequencyType === 1 ? 'weeks' : frequencyType === 2 ? 'months' : frequencyType === 3 ? 'years' : null;
-                let earliestScanDate = moment(currentDate).subtract(1, frequencyTypeString);
-                let lastScanToday = component.lastScan !== undefined && moment(component.lastScan.time).isSame(currentDate, 'day');
+                actionRequiredComponents.push({
+                    date: formattedDay,
+                    data: [],
+                });
 
-                Scan.countDocuments(
-                    {
-                        equipment: component.equipment,
-                        component: component._id,
-                        time: {
-                            $gte: moment(earliestScanDate).toDate(),
-                            $lte: moment(currentDate).toDate()
-                        },
-                    }
-                ).exec(function (err, count) {
-                    if (err)
-                        return console.log('err: ' + err);
+                components.map(function (component, i) {
+                    let frequency = component.frequency;
+                    let frequencyType = component.frequencyType;
+                    let frequencyDays = component.frequencyDays;
+                    let frequencyTypeString = frequencyType === 0 ? 'days' : frequencyType === 1 ? 'weeks' : frequencyType === 2 ? 'months' : frequencyType === 3 ? 'years' : null;
+                    let earliestScanDate = moment(currentDate).subtract(1, frequencyTypeString);
+                    let lastScanToday = component.lastScan !== undefined && moment(component.lastScan.time).isSame(currentDate, 'day');
+                    let scanCount = scans.filter(x => x.equipment === component.equipment && x.time > earliestScanDate && x.time < currentDate).length;
 
-                    if (count < frequency && !lastScanToday && (frequencyDays.length === 0 || frequencyDays.includes(currentDay))) {
-                        actionRequiredComponents.find(x => x.date === formattedDay).data.push(component);
+                    if (scanCount < frequency && (frequencyDays.length === 0 || frequencyDays.includes(currentDay))) {
+                        let foundComponent = actionRequiredComponents.find(x => x.date === formattedDay);
+                        let clonedComponent = Object.assign({}, component);
+
+                        if (lastScanToday)
+                            clonedComponent.scanStatus = component.lastScan.status;
+
+                        foundComponent.data.push(clonedComponent);
                     }
 
                     if (i === components.length - 1 && !moment(currentDate).add(1, 'days').isBefore(moment(endDate))) {
                         return res.send(JSON.stringify(actionRequiredComponents));
                     }
                 });
-            });
-        }
+            }
+        });
     });
 });
 
