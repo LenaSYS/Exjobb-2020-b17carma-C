1649d0e5b8a4db8253bc3bd5ae22c4edcaa8cff1
b17carma <b17carma@student.his.se>
2020-03-10 00:28:13 +0100

    Add another potential fix for CORS issue

--- src/index.js
+++ src/index.js
@@ -16,12 +16,7 @@ app.use(function (err, req, res) {
     res.status(500).send('Internal server error')
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://api.carlmaier.se");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/equipment', equipment);
