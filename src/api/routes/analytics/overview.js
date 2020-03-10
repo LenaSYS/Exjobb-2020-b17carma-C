65d687ddc42cbde1210f5b98027517e96fa4753f
b17carma <b17carma@student.his.se>
2020-03-10 18:06:15 +0100

    add debug message

--- src/api/routes/analytics/overview.js
+++ src/api/routes/analytics/overview.js
@@ -20,6 +20,7 @@ function overview(req, res) {
             for (let m = moment(startDate); m.isBefore(endDate); m.add(1, 'days')) {
                 let currentDate = moment(m);
                 let currentDay = currentDate.isoWeekday();
+                console.log(currentDate.format("DD-MM-YY dddd - ISOWEEKDAY: " + currentDay + " NONISO: " + currentDate.day()));
 
                 let saveObject = {
                     date: currentDate,
