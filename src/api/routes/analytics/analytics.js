6cdfd7cc40abed15aa963b561700e054c3962718
b17carma <b17carma@student.his.se>
2020-03-19 15:14:16 +0100

    Add experimental native chart data

--- src/api/routes/analytics/analytics.js
+++ src/api/routes/analytics/analytics.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pieChart = require('./charts/piechart');
 const scans = require('./scans');
 const lineChart = require('./charts/linechart');
+const lineChartNative = require('./charts/linechartnative');
 const calendarChart = require('./charts/calendarchart');
 const overview = require('./overview');
 
@@ -10,6 +11,7 @@ const router = express.Router();
 router.get('/stats', pieChart);
 router.get('/scans', scans);
 router.get('/scans/:equipmentId/:startDate/:endDate', lineChart);
+router.get('/scans/:equipmentId/:startDate/:endDate/native', lineChartNative);
 router.get('/calendar', calendarChart);
 router.get("/overview/:startDate/:endDate", overview);
 
---
+++ src/api/routes/analytics/charts/linechartnative.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const moment = require('moment-timezone');
+
+const Scan = require("../../../mongodb/schema/Scan");
+
+function lineChart(req, res) {
+    mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
+
+    let startDate = moment(req.params.startDate);
+    let endDate = moment(req.params.endDate);
+
+    let filter = {
+        equipmentId: req.params.equipmentId,
+        time: {
+            $gte: moment(req.params.startDate).toDate(),
+            $lte: moment(req.params.endDate).toDate()
+        },
+    };
+
+    let container = {
+        labels: [],
+        datasets: [
+            { //Normal
+                data: []
+            },
+            { //Faulty
+                data: []
+            }
+        ]
+    };
+
+    Scan.find(filter).sort({time: 'ascending'}).lean().exec(function (err, scans) {
+        for (let m = moment(startDate); m.isBefore(endDate); m.add(1, 'weeks')) {
+            let currentDate = moment(m);
+            let formattedWeek = m.day('Monday').format("DD/MM/YY");
+
+            container.labels.push(formattedWeek);
+
+            let matchingScans = scans.find(x => moment(x.time).isSame(currentDate, 'week'));
+
+            let numNormal = matchingScans.filter(scan => scan.status === true);
+            let numFaulty = matchingScans.filter(scan => scan.status === false);
+
+            container.datasets[0].data.push(numNormal);
+            container.datasets[1].data.push(numFaulty);
+
+        }
+        return res.send(JSON.stringify(container));
+    });
+}
+
+module.exports = lineChart;
