82bdb3dddf328f32698a1ee338997acee2bb0bba
b17carma <b17carma@student.his.se>
2020-03-19 15:49:01 +0100

    Fix issue with line chart colors

--- src/api/routes/analytics/charts/linechartnative.js
+++ src/api/routes/analytics/charts/linechartnative.js
@@ -22,11 +22,11 @@ function lineChart(req, res) {
         datasets: [
             { //Normal
                 data: [],
-                color: (opacity = 1) => `rgba(232, 193, 160, ${opacity})`,
+                color: "(opacity = 1) => `rgba(232, 193, 160, ${opacity})`",
             },
             { //Faulty
                 data: [],
-                color: (opacity = 1) => `rgba(244, 117, 96, ${opacity})`,
+                color: "(opacity = 1) => `rgba(244, 117, 96, ${opacity})`",
             }
         ]
     };
